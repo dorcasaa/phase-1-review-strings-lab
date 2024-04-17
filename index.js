183cdea9d7b52ff3028ebafafcb6daafd24865f0
dorcas <dorcasaumaakamu@gmail.com>
2024-04-17 17:06:38 +0300

    message

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your code in this file!
+// index.js
+
+// Define currentUser variable
+const currentUser = 'Grace Hopper';
+
+// Define welcomeMessage variable
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Define excitedWelcomeMessage variable
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// Define shortGreeting variable
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+module.exports = {
+  currentUser,
+  welcomeMessage,
+  excitedWelcomeMessage,
+  shortGreeting
+};
